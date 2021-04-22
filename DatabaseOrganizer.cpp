6aa33aa99ae3bb3bff6f4b71e5faf7f0c420c7d1
Benlee1000 <benny01lee@gmail.com>
2021-04-21 17:45:14 -0700

    Added a working swap function in database organizer to organize data later.

--- DatabaseOrganizer.cpp
+++ DatabaseOrganizer.cpp
@@ -11,3 +11,32 @@ int DatabaseOrganizer::findState(string stateName, Database &database)
 	}
 	return -1;
 }
+
+void DatabaseOrganizer::swapState(State& state1, State& state2)
+{
+	tempState = copyState(state1);
+	replaceState(state1, state2);
+	replaceState(state2, tempState);
+}
+
+void DatabaseOrganizer::replaceState(State& state1, State& state2)
+{
+	state1.setState(state2.getState());
+	state1.setDosesDelivered(state2.getDosesDelivered());
+	state1.setDosesDeliveredPer100k(state2.getDosesDeliveredPer100k());
+	state1.setDosesAdministered(state2.getDosesAdministered());
+	state1.setDosesAdministeredPer100k(state2.getDosesAdministeredPer100k());
+	state1.setPercentOfPopWithAtLeastOneDose(state2.getPercentOfPopWithAtLeastOneDose());
+}
+
+State DatabaseOrganizer::copyState(State& state)
+{
+	tempCopyState.setState(state.getState());
+	tempCopyState.setDosesDelivered(state.getDosesDelivered());
+	tempCopyState.setDosesDeliveredPer100k(state.getDosesDeliveredPer100k());
+	tempCopyState.setDosesAdministered(state.getDosesAdministered());
+	tempCopyState.setDosesAdministeredPer100k(state.getDosesAdministeredPer100k());
+	tempCopyState.setPercentOfPopWithAtLeastOneDose(state.getPercentOfPopWithAtLeastOneDose());
+
+	return tempCopyState;
+}
--- DatabaseOrganizer.h
+++ DatabaseOrganizer.h
@@ -10,7 +10,20 @@ class DatabaseOrganizer
 private:
 	int stateSearchIndex;
 
+	// Store temporary states, for swapping states
+	State tempState;
+	State tempCopyState;
+
 public:
 	int findState(string stateName, Database &database);
+
+	// Swap state1 and state2
+	void swapState(State &state1, State &state2);
+
+	// Replace the information in state1 with state2
+	void replaceState(State &state1, State &state2);
+
+	// Return a copy of a state
+	State copyState(State& state);
 };
 
